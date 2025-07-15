663a8b776eccce513b7dde90d8726cd6b008b427
Umugishamarket <umugishamarket@gmail.com>
2025-07-15 14:12:28 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
-// firebaseConfig.js — ikoreshwa muri <script src="firebaseConfig.js">
+// firebaseConfig.js
 
 const firebaseConfig = {
   apiKey: "AIzaSyCxKjaqkSEsFY9fk5Y_XcN3viSQLX5S2Kw",
   authDomain: "adepr-system.firebaseapp.com",
   projectId: "adepr-system",
-  storageBucket: "adepr-system.appspot.com", // 🔧 Hano harakosowe
+  storageBucket: "adepr-system.appspot.com",
   messagingSenderId: "370773084410",
   appId: "1:370773084410:web:e89dfd42c6fecdc5222143",
   measurementId: "G-J2ZGZ1EF54"
@@ -12,7 +12,5 @@ const firebaseConfig = {
 
 // Tangiza Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Kurinda authentication na database
 const auth = firebase.auth();
 const db = firebase.firestore();
